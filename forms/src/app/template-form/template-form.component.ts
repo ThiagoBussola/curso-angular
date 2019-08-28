7c949aad2e26c9694cb953449e3f00be50e24da9
thiago <thiagobussola@hotmail.com>
2019-08-28 14:58:57 -0300

    Rotas e Formularios

---
+++ forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-template-form',
+    templateUrl: './template-form.component.html',
+    styleUrls: ['./template-form.component.scss']
+})
+export class TemplateFormComponent implements OnInit {
+
+    usuario: any = {
+        nome: null,
+        email: null
+    }
+
+    
+    constructor() { }
+    
+    ngOnInit() {
+    }
+
+    onSubmit(form) {
+        console.log(form);
+
+        
+        
+    } 
+}
---
+++ forms/src/app/template-form/template-form.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { TemplateFormComponent } from './template-form.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule
+    ],
+
+    declarations: [
+        TemplateFormComponent,
+        FormDebugComponent
+    ],
+})
+export class TemplateFormModule { }
